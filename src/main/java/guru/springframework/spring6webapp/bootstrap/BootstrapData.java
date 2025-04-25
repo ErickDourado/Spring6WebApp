2e0481a137d7de375c3a47e4d679c89e56466280
Erick Dourado <erickdourado_@hotmail.com>
2025-04-24 22:26:38 -0300

    adding publisher

--- src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring6webapp.bootstrap;
 
 import guru.springframework.spring6webapp.domain.Author;
 import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.domain.Publisher;
 import guru.springframework.spring6webapp.repositories.AuthorRepository;
 import guru.springframework.spring6webapp.repositories.BookRepository;
+import guru.springframework.spring6webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,14 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository,
+                         BookRepository bookRepository,
+                         PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -48,9 +54,27 @@ public class BootstrapData implements CommandLineRunner {
         authorRepository.save(ericSaved);
         authorRepository.save(rodSaved);
 
+        Publisher publisherOne = new Publisher();
+        publisherOne.setPublisherName("Publisher name");
+        publisherOne.setAddress("123 Main St");
+        publisherOne.setCity("Springfield");
+        publisherOne.setState("Sao Paulo");
+        publisherOne.setZipCode("01587682");
+
+        Publisher publisherTwo = new Publisher();
+        publisherOne.setPublisherName("Publisher two name");
+        publisherOne.setAddress("456 Main St");
+        publisherOne.setCity("Springfield");
+        publisherOne.setState("Rio de Janeiro");
+        publisherOne.setZipCode("06879054");
+
+        publisherRepository.save(publisherOne);
+        publisherRepository.save(publisherTwo);
+
         System.out.println("In Bootstrap");
         System.out.println("Author Count: " + authorRepository.count());
         System.out.println("Book Count: " + bookRepository.count());
+        System.out.println("Publisher Count: " + publisherRepository.count());
     }
 
 }
